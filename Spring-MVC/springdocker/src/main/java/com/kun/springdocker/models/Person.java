5ac79b143ec25b0d5c72e0843c906353d7cd722f
gilangrkun <gilang.grk17@gmail.com>
2020-05-25 08:50:19 +0700

    Update springdocker project

--- Spring-MVC/springdocker/src/main/java/com/kun/springdocker/models/Person.java
+++ Spring-MVC/springdocker/src/main/java/com/kun/springdocker/models/Person.java
@@ -2,11 +2,15 @@ package com.kun.springdocker.models;
 
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Person {
 
     public final UUID id;
+
+    @NotBlank
     public final String name;
 
     public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
