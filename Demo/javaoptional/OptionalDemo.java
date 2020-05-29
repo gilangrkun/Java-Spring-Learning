d3bdd4200d3fc49eb4c28224326c0c198cb1b928
gilangrkun <gilang.grk17@gmail.com>
2020-05-29 17:42:55 +0700

    Update javaoptional | 29th May 20, 17:42

--- Demo/javaoptional/OptionalDemo.java
+++ Demo/javaoptional/OptionalDemo.java
@@ -5,28 +5,28 @@ public class OptionalDemo {
 
         // Optional<String> hello = Optional.ofNullable("hello Japan");
         // Optional<String> hello = Optional.of(null);
-        Optional<String> hello = Optional.ofNullable("hello world");
+        // Optional<String> hello = Optional.ofNullable("hello world");
 
         // System.out.println("\n" + hello.isEmpty());
         // System.out.println(hello.isPresent());
-        System.out.print("\n1. ");
-        hello.ifPresentOrElse(present -> {
-            System.out.println(present);
-        }, () -> {
-            System.out.println("hello UK");
-        });
+        // System.out.print("\n1. ");
+        // hello.ifPresentOrElse(present -> {
+        // System.out.println(present);
+        // }, () -> {
+        // System.out.println("hello UK");
+        // });
 
-        Optional<String> hello2 = Optional.ofNullable(null);
+        // Optional<String> hello2 = Optional.ofNullable(null);
 
         // System.out.println("\n" + hello.isEmpty());
         // System.out.println(hello.isPresent());
 
-        System.out.print("\n2. ");
-        hello2.ifPresentOrElse(present -> {
-            System.out.println(present);
-        }, () -> {
-            System.out.println("hello UK");
-        });
+        // System.out.print("\n2. ");
+        // hello2.ifPresentOrElse(present -> {
+        // System.out.println(present);
+        // }, () -> {
+        // System.out.println("hello UK");
+        // });
 
         /*
          * if optional contain null, then orElse is used. ex output -> "hello Indonesia"
@@ -41,5 +41,44 @@ public class OptionalDemo {
          * System.out.printf("\n%s", orElse);
          */
 
+        // if users input completely
+        Data data = new Data("Alexa", "gorgeous.alexa@gmail.com", "@alexis");
+        String email = data.getEmail().map(String::toLowerCase).orElse("email is not given");
+        String twitter = data.getTwitterAcc().map(String::toLowerCase).orElse("twitter account is not given");
+
+        // if users are not input completely
+        Data data2 = new Data("Alexa", null, null);
+        String email2 = data2.getEmail().map(String::toLowerCase).orElse("email is not given");
+        String twitter2 = data2.getTwitterAcc().map(String::toLowerCase).orElse("twitter account is not given");
+
+        System.out.println("\nemail1  :\t" + email);
+        System.out.println("\nemail2  :\t" + email2);
+        System.out.println("\ntwitter1:\t" + twitter);
+        System.out.println("\ntwitter2:\t" + twitter2);
+    }
+}
+
+class Data {
+
+    private final String name;
+    private final String email;
+    private final String twitterAcc;
+
+    public Data(String name, String email, String twitterAcc) {
+        this.name = name;
+        this.email = email;
+        this.twitterAcc = twitterAcc;
+    }
+
+    public Optional<String> getTwitterAcc() {
+        return Optional.ofNullable(twitterAcc);
+    }
+
+    public Optional<String> getEmail() {
+        return Optional.ofNullable(email);
+    }
+
+    public String getName() {
+        return name;
     }
 }
