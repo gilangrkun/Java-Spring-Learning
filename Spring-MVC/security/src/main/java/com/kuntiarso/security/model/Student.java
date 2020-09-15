abedbbe0add5444b6f6b9bacb39a593692420a96
kuntiarso <gilang.grk17@gmail.com>
2020-09-16 01:45:49 +0700

    Added new folder spring-security demo

---
+++ Spring-MVC/security/src/main/java/com/kuntiarso/security/model/Student.java
@@ -0,0 +1,10 @@
+package com.kuntiarso.security.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    
+    private final Integer studentId;
+    private final String studentName;
+}
---
+++ Spring-MVC/security/src/test/java/com/kuntiarso/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuntiarso.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
